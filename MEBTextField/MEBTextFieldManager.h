51ff0d6dc277a682b0fb3d6085faa4d6b9d1eb6a
gaodeying <964665684@qq.com>
2018-05-03 18:55:45 +0800

    添加manager

---
+++ MEBTextField/MEBTextFieldManager.h
@@ -0,0 +1,13 @@
+//
+//  MEBTextFieldManager.h
+//  MEBTextFiledDemo
+//
+//  Created by 搞得赢 on 2018/5/3.
+//  Copyright © 2018年 德赢工作室. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MEBTextFieldManager : NSObject
+
+@end
---
+++ MEBTextField/MEBTextFieldManager.m
@@ -0,0 +1,13 @@
+//
+//  MEBTextFieldManager.m
+//  MEBTextFiledDemo
+//
+//  Created by 搞得赢 on 2018/5/3.
+//  Copyright © 2018年 德赢工作室. All rights reserved.
+//
+
+#import "MEBTextFieldManager.h"
+
+@implementation MEBTextFieldManager
+
+@end
